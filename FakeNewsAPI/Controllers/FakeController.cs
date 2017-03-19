f7f28e0c0395d68348f21d17b3b754a615cd7d70
Jamie Lord <jamie.lord@ifsworld.com>
2017-03-19 07:43:04 +0000

    Fake endpoint working

--- FakeNewsAPI/Controllers/FakeController.cs
+++ FakeNewsAPI/Controllers/FakeController.cs
@@ -1,13 +1,16 @@
-﻿using System.Linq;
+﻿using FakeNewsAPI.Models;
+using System.Data;
+using System.Linq;
 using System.Web.Http;
 
 namespace FakeNewsAPI.Controllers
 {
     public class FakeController : ApiController
     {
-        public IQueryable<NewsResponse> GetFakeNews()
+        private readonly FakeNewsAPIContext db = new FakeNewsAPIContext();
+        public IQueryable<News> GetFakeNews()
         {
-            return null;
+            return db.News.Where(n => n.Score > 0).OrderBy(n => n.Score).Take(30);
         }
     }
 }
